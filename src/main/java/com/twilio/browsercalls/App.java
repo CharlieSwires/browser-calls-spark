98a013c4db067162c16ad6ec5fdf2cf595954a34
charles_swires <charles.swires@vizionnetwork.co.uk>
2020-02-20 16:50:22 +0000

    Initial

--- src/main/java/com/twilio/browsercalls/App.java
+++ src/main/java/com/twilio/browsercalls/App.java
@@ -12,7 +12,11 @@ import javax.persistence.EntityManagerFactory;
 
 import static spark.Spark.*;
 import static spark.Spark.afterAfter;
-
+import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 /**
  * Main application class. The environment is set up here, and all necessary services are run.
  */
@@ -52,7 +56,32 @@ public class App {
     post("/call/connect", new CallController().connect);
     post("/ticket/create", new TicketController(ticketService).create,
         new MustacheTemplateEngine());
-
+//    Thread t = new Thread(){
+//    	@Override
+//    	public void run(){
+    		try{
+    			InetAddress ip = InetAddress.getLocalHost();
+    			String command = "ngrok http -subdomain=twiliovizionnetwork"+ip.getHostName()+" 4567";
+//    			String command = "ngrok http 4567";
+//    			Process process = Runtime.getRuntime().exec(command, null, null);
+//    			StringBuilder output = new StringBuilder();
+//
+//    			BufferedReader reader = new BufferedReader(
+//    					new InputStreamReader(process.getInputStream()));
+//
+//    			String line;
+//    			while ((line = reader.readLine()) != null) {
+//    				output.append(line + "\n");
+//    			}
+    			System.out.println(command);
+//    			System.out.println(output);
+    		} catch (Exception ex){
+    			ex.printStackTrace();
+    		}
+//    	}
+//    };
+//    t.start();
     afterAfter(new LoggingFilter());
+
   }
 }
